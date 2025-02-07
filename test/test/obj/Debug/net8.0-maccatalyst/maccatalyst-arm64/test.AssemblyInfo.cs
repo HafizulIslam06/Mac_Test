39a06aee3230b5c1387bcee3ee80dc123d45309d
hafizulislam <mdhafizul3131@gmail.com>
2025-02-07 23:57:54 +0600

    changed

--- test/test/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/test.AssemblyInfo.cs
+++ test/test/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fa57b3c36509a9e32027974c1b7d968d35f410b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
